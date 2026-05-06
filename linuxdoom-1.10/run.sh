c27c2a18bf04b2fa3ee30d255bea2590ee64f91e
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-05-06 11:33:20 +0800

    update run.sh

--- linuxdoom-1.10/run.sh
+++ linuxdoom-1.10/run.sh
@@ -1,4 +1,15 @@
-sudo -b Xephyr :2 -ac -screen 640x400x8
-sleep 1 # hack
-DISPLAY=:2
-./linux/linuxxdoom -2
+#!/bin/sh
+
+# sample script for starting linux doom
+
+DISPLAY_NO=2
+DISPLAY_MODE=640x400x8
+DELAY=1 # hack
+DOOM=./linux/linuxxdoom
+
+sudo -b Xephyr :$DISPLAY_NO -ac -screen $DISPLAY_MODE
+sleep $DELAY # hack
+printf '\n'  # hack
+DISPLAY=:$DISPLAY_NO $DOOM -$DISPLAY_NO
+printf '\n'  # hack
+sleep $DELAY # hack
