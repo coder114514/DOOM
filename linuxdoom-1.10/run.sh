88c7e69e0b5d4f11dfb938ed06117769fd784d8c
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-09-09 02:48:26 +0800

    no need to start Xephyr as root

--- linuxdoom-1.10/run.sh
+++ linuxdoom-1.10/run.sh
@@ -7,7 +7,7 @@ DISPLAY_MODE=640x400x8
 DELAY=1 # hack
 DOOM=./linux/linuxxdoom
 
-sudo -b Xephyr :$DISPLAY_NO -ac -screen $DISPLAY_MODE
+Xephyr :$DISPLAY_NO -ac -screen $DISPLAY_MODE &
 sleep $DELAY # hack
 printf '\n'  # hack
 DISPLAY=:$DISPLAY_NO $DOOM -$DISPLAY_NO
